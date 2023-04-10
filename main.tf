4ba8ab6f6fefbdae2adf639fd5087b25ca37dc5d
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 10:13:59 +0530

    updating ./tf-module-rds

--- main.tf
+++ main.tf
@@ -1,12 +1,32 @@
-resource "aws_db_instance" "default" {
-  for_each             = var.rds
-  allocated_storage    = each.value.allocated_storage
-  db_name              = "${var.env}-${each.key}-roboshop-rds"
-  engine               = each.value.engine
-  engine_version       = each.value.engine_version
-  instance_class       = each.value.instance_calss
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = each.value.parameter_group_name
-  skip_final_snapshot  = true
-} 
+
+
+resource "aws_rds_cluster" "default" {
+  for_each            = var.rds
+  cluster_identifier  = "${var.env}-${each.key}-roboshop-rds"
+  engine              = each.value.engine
+  engine_version      = each.value.engine_version
+  database_name       = "mydb"
+  master_username     = "foo"
+  master_password     = "cjnsdfnjn"
+  skip_final_snapshot = true
+  db_cluster_parameter_group_name = "${var.env}-${each.key}-roboshop-rds"
+  db_subnet_group_name = "${var.env}-${each.key}-roboshop-rds"
+}
+
+resource "aws_rds_cluster_parameter_group" "default" {
+  for_each = var.rds
+  name        = "${var.env}-${each.key}-roboshop-rds"
+  family      = "aurora5.7"
+  description = "RDS default cluster parameter group"
+
+}
+
+resource "aws_db_subnet_group" "default" {
+  for_each   = var.rds
+  name       = "${var.env}-${each.key}-roboshop-rds"
+  subnet_ids = var.subnets
+
+  tags = {
+    Name = "${var.env}-${each.key}-roboshop-rds"
+  }
+}

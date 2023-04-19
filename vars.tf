1f0a8641175a416e87b630bfefc567620ea126f5
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-19 17:34:27 +0530

    updating ./tf-module-rds

--- vars.tf
+++ vars.tf
@@ -1,9 +1,10 @@
 variable "env" {}
+variable "rds"{}
 variable "subnets" {}
 variable "name" {}
 variable "allocated_storage" {}
 variable "engine" {}
 variable "engine_version" {}
 variable "instance_class" {}
-#variable "parameter_group_name" {}
+variable "parameter_group_name" {}
 variable "skip_final_snapshot" {}
